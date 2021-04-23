58d0a8e5257e986f6796e6785f8cc19c5573eccc
weedskimhj <kimhj@weedscomm.com>
2021-04-23 10:47:24 +0900

    sequelize 공식문서 참조.

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const path = require('path')
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config.json')[env]
 
 
-const Sequelize = 
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect
+  });
 
 
 const app = express()
+
+async function connectDB() {
+    try {
+        await sequelize.authenticate()
+        console.log('Connection has been established successfully');
+    } catch(err) {
+        console.error('Unable to connect to the databse: ', err);
+    }
+}
+
+connectDB()
+
+
 
 app.get('/', (req, res) => res.send('INDEX'))
 
